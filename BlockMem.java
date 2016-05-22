c0ce9968a5d45cb7152e1d0152b5a713cb190eab
espeto <gkreisler5@hotmail.com>
2016-05-22 01:01:29 +0100

    Classe referente ao Bloco de Memoria da Cache

---
+++ BlockMem.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulador.cache;
+
+/**
+ *
+ * @author jaqueline
+ */
+public class BlockMem {
+    private int _nsize;
+    private byte[] _data;
+    
+    public BlockMem(){
+        _nsize = 4;
+        _data = new byte[4];
+    }
+    
+    public BlockMem(int size){
+        _nsize = size;
+        _data = new byte[size];
+    }
+    
+    private void setBlock(){
+        
+    }
+    
+    private void getBlock(){
+        
+    }
+}

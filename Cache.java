86ae5ea9c4e9db2dc4d37bd6d8d95bb88b219123
Gilberto kreisler <Gilberto kreisler>
2016-06-03 14:22:38 +0100

    Tratamento de Miss. Implementado escrita randomica. Adicao de variavel contadora no metodo read. Variavel contadora de hit e total de acessos.

--- Cache.java
+++ Cache.java
@@ -17,7 +17,7 @@ public abstract class Cache {
 
     private BlockMem _cache_blocks[][];
     private int _val[][];
-    private int _tag[];
+    private int _tag[][];
     private int _nblock; //Numero de blocos na cache
     private int _nsets; //Numero de entradas na cache
     private int _bsize; //Tamanho do bloco
@@ -25,7 +25,9 @@ public abstract class Cache {
     private int _nbits_offset; //Numero bits Offset
     private int _nbits_indice; //Numero bits Indice
     private int _nbits_tag;    //Numero bits Tag
-    private int _conf_miss, _cap_miss, _comp_miss; //Miss: Conflito, Capacidade, Compulsório
+    private int _conf_miss = 0, _cap_miss = 0, _comp_miss = 0; //Miss: Conflito, Capacidade, Compulsório
+    private int _hit = 0;
+    private int _total_access = 0;
 
     /* public Cache() {
      _nblock = 1024;
@@ -38,6 +40,7 @@ public abstract class Cache {
      calcBits();
 
      }*/
+    
     //As entradas podem ser feitas de forma básica.
     //Antes de criar o objeto a gente define na SimuladorCache
     //os valores padrão
@@ -52,7 +55,7 @@ public abstract class Cache {
         _nsets = _nblock / _ass; //A partir do número de blocos na cache é calculado o número de sets.
         _bsize = bsize;
         _val = new int[_nsets][_ass];
-        _tag = new int[_nsets];
+        _tag = new int[_nsets][_ass];
         _cache_blocks = new BlockMem[_nsets][_ass];
         calcBits();
     }
@@ -74,38 +77,58 @@ public abstract class Cache {
     }
 
     /**
-     * @see Implementação da leitura da cache
+     * @see Implementa a leitura da cache
      * @param end integer - Endereço passado
      */
-    private void read(int end) {
+    public void read(int end) {
         int indice = (end / _nbits_offset) & (2 ^ (_nbits_indice - 1));
         int tag = (end / (_nbits_offset + _nbits_indice));
-        
-        //Alterar lógica para matriz
-        if (_val[indice] == 0) {
-            //Gera miss compulsorio
+        int invalCount = 0;//Contador dos bits de validade. Se val = 0 -> contador++
+
+        //Ao chamar o método de leitura já é contabilizado um acesso
+        _total_access++;
+
+        for (int i = 0; i < _ass; i++) {
+
+            if (_val[indice][i] == 0) {
+                //Gera miss compulsorio
+                //Miss compulsório será contabilizado ao final
+                //Se em toda associatividade a validade for 0
+                invalCount++;
+            } else {
+                if (_tag[indice][i] == tag) {
+                    //Hit
+                    _hit++;
+                    return;
+                } else {
+                    if (i == _ass - 1) {
+                        //Miss de conflito
+                        _conf_miss++;
+                        write(end);
+                    }
+
+                }
+            }
         }
-        else{
-            if (_tag[indice] == tag) {
-                //Hit
-            }
-            else{
-                //Miss de conflito
-                write(end);
-                
-            }
+
+        if (invalCount == _ass) {
+            _comp_miss++;
         }
     }
 
     //Escrita feita após miss de leitura
-    private void write(int end) {
+    public void write(int end) {
         int indice = (end / _nbits_offset) & (2 ^ (_nbits_indice - 1));
         int tag = (end / (_nbits_offset + _nbits_indice));
-        
-        //Alterar lógica para matriz
-        if (_val[indice] == 0) {
+        Random rand = null;
+        int random_number = rand.nextInt(_ass);
 
-        }
+        //Ao chamar a escrita já é contabilizado um acesso
+        _total_access++;
+
+        //Escrita feita em posição randomica
+         _val[indice][random_number] = 1;
+         _tag[indice][random_number] = tag;
     }
 
 }
